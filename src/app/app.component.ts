0ad8dda81c42dc2478759bf5ea0d5fc04be2b136
junmeen <junaid.ameen@wipro.com>
2018-07-31 15:40:49 +0530

    Bootstrap3 and Jquery Support added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery' ;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('I\'ve been Initialized');
+  }
+
+  buttonClicked(): void {
+    alert('Hello World');
+  }
+
 }
