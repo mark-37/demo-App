69afab97ed089a51a70a02d2ecc7c3bfc2d05fba
junmeen <junaid.ameen@wipro.com>
2018-08-02 10:07:43 +0530

    Employee details Added

--- src/app/employee-count/employee-count.component.ts
+++ src/app/employee-count/employee-count.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-employee-count',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeCountComponent implements OnInit {
 
+  @Input()
+  all: number ;
+
+  @Input()
+  male: number;
+
+  @Input()
+  female: number;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -8,18 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class EmployeeListComponent implements OnInit {
 
   Employees: any[] = [
-    { code: 'emp101', name: 'Arun Jain', gender: 'male', annualSalary: 1000, dateOfBirth: '16/06/1996' },
-    { code: 'emp102', name: 'Rahul Ranjan', gender: 'male', annualSalary: 1100, dateOfBirth: '17/07/1996' },
-    { code: 'emp103', name: 'Rishabh Verma', gender: 'male', annualSalary: 1200, dateOfBirth: '18/08/1996' },
-    { code: 'emp104', name: 'Jenny White', gender: 'female', annualSalary: 1300, dateOfBirth: '14/02/1997' },
-    { code: 'emp105', name: 'Shane Doe', gender: 'female', annualSalary: 1400, dateOfBirth: '12/02/1995' }
+    { code: 'emp100', name: 'Arun Jain', gender: 'Male', annualSalary: 1000, dateOfBirth: '16/06/1996' },
+    { code: 'emp101', name: 'Shaila Kapoor', gender: 'Female', annualSalary: 1010, dateOfBirth: '15/05/1993' },
+    { code: 'emp102', name: 'Rahul Ranjan', gender: 'Male', annualSalary: 1100, dateOfBirth: '17/07/1996' },
+    { code: 'emp103', name: 'Rishabh Verma', gender: 'Male', annualSalary: 1200, dateOfBirth: '18/08/1996' },
+    { code: 'emp104', name: 'Jenny White', gender: 'Female', annualSalary: 1300, dateOfBirth: '14/02/1997' },
+    { code: 'emp105', name: 'Shane Doe', gender: 'Female', annualSalary: 1400, dateOfBirth: '12/02/1995' }
   ];
 
   constructor() { }
 
   ngOnInit() {
   }
+  
+  getTotalEmployeesCount(): number {
+    return this.Employees.length;
+  }
 
+  getTotalFemaleEmployeesCount(): number {
+    return this.Employees.filter(e => e.gender === 'Female').length;
+  }
+
+  getTotalMaleEmployeesCount(): number {
+    return this.Employees.filter(e => e.gender === 'Male').length;
+  }
 
 
 }
