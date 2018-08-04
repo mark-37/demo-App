55b82d7cf00481b8822ecc5c7a65773d2fcca614
mark-37 <safdaransari37@gmail.com>
2018-08-05 01:33:14 +0530

    Added Employee Service support

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,28 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { IEmployee } from '../employee';
+import { EmployeeService } from '../employee/employee.service';
 
 @Component({
   selector: 'app-list-employee',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
+  styleUrls: ['./employee-list.component.css'],
+  providers: [ EmployeeService ]
 })
 export class EmployeeListComponent implements OnInit {
 
   selectedEmployeeCountRadioButtonValue: string = 'All';
 
-  Employees: IEmployee[] = [
-    { code: 'emp100', name: 'Arun Jain', gender: 'Male', annualSalary: 1000, dateOfBirth: '16/06/1996' },
-    { code: 'emp101', name: 'Shaila Kapoor', gender: 'Female', annualSalary: 1010, dateOfBirth: '15/05/1993' },
-    { code: 'emp102', name: 'Rahul Ranjan', gender: 'Male', annualSalary: 1100, dateOfBirth: '17/07/1996' },
-    { code: 'emp103', name: 'Rishabh Verma', gender: 'Male', annualSalary: 1200, dateOfBirth: '18/08/1996' },
-    { code: 'emp104', name: 'Jenny White', gender: 'Female', annualSalary: 1300, dateOfBirth: '14/02/1997' },
-    { code: 'emp105', name: 'Shane Doe', gender: 'Female', annualSalary: 1400, dateOfBirth: '12/02/1995' },
-    { code: 'emp106', name: 'Sonam Kapoor', gender: 'Female', annualSalary: 1500, dateOfBirth: '13/01/1982' },
-  ];
+  Employees: IEmployee[];
 
-  constructor() { }
+  constructor( private _employeeService : EmployeeService) { }
 
   ngOnInit() {
+    this.Employees = this._employeeService.getEmployees();
   }
 
   onEmployeeCountRadioButtonChange(selectedRadioButtonValue: string): void {
---
+++ src/app/employee/employee.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { IEmployee } from '../employee';
+
+@Injectable()
+
+export class EmployeeService {
+
+  constructor() { }
+
+  getEmployees(): IEmployee[] {
+  	return [
+  		{ code: 'emp100', name: 'Arun Jain', gender: 'Male', annualSalary: 1000, dateOfBirth: '16/06/1996' },
+    { code: 'emp101', name: 'Shaila Kapoor', gender: 'Female', annualSalary: 1010, dateOfBirth: '15/05/1993' },
+    { code: 'emp102', name: 'Rahul Ranjan', gender: 'Male', annualSalary: 1100, dateOfBirth: '17/07/1996' },
+    { code: 'emp103', name: 'Rishabh Verma', gender: 'Male', annualSalary: 1200, dateOfBirth: '18/08/1996' },
+    { code: 'emp104', name: 'Jenny White', gender: 'Female', annualSalary: 1300, dateOfBirth: '14/02/1997' },
+    { code: 'emp105', name: 'Shane Doe', gender: 'Female', annualSalary: 1400, dateOfBirth: '12/02/1995' },
+    { code: 'emp106', name: 'Sonam Kapoor', gender: 'Female', annualSalary: 1500, dateOfBirth: '13/01/1982' },
+  	];
+  }
+}
